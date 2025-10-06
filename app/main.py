f71c69f3d30bc02683035ae720b917b9f9852733
mourfp <mourefp@gmail.com>
2025-10-06 20:07:45 +0200

    Corregido fondo desplazamiento, for de tipos, descarga de imagenes de tipos

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
-from flask import Flask, current_app, json, jsonify, render_template; 
-import datetime 
+from flask import Flask, current_app, json, jsonify, render_template
+import datetime
 
 app = Flask(__name__, template_folder='templates')
 
 with open("./data/data.json", encoding="utf-8") as fichero_data:
-    app.config["data"]=json.load(fichero_data)
+    app.config["data"] = json.load(fichero_data)
+
 
 @app.route('/')
 # def hello_world():
 #     return 'Hello, World!'
 def welcome():
-    year=datetime.datetime.now().year
+    year = datetime.datetime.now().year
     return render_template('index.html', year=year)
+
 
 @app.route("/file")
 def file_json():
@@ -21,11 +23,22 @@ def file_json():
 #     year=datetime.datetime.now().year
 #     return render_template('index.html', year=year)
 
+
 @app.route("/pokemons/")
 def pokemon_list():
-    year=datetime.datetime.now().year
+    year = datetime.datetime.now().year
     pokemons = app.config["data"]
-    return render_template("pokemon_list.html", year=year,pokemons=pokemons)
+    # type_image = {
+    #     'fyre': "app\static\images\Types\Type_Fuego.png",
+    #     'flying': 'app\static\images\Types\Type_Volador.png',
+    #     'grass': 'app\static\images\Types\Type_Planta.png',
+    #     'poison': 'app\static\images\Types\Type_Veneno.png',
+    #     'water': 'app\static\images\Types\Type_Agua.png',
+    #     'electric': 'app\static\images\Types\Type_Eléctrico.png'
+    # }
+
+    return render_template("pokemon_list.html", year=year, pokemons=pokemons)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8080, debug="True")
