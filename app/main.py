b8ca58b39da81008b6af544bd7c582d274a47706
mourfp <mourefp@gmail.com>
2025-10-17 19:16:59 +0200

    Estilado pagina battle

--- app/main.py
+++ app/main.py
@@ -139,50 +139,49 @@ def pokemon_battle():
             'sprite': url_for('static', filename='images/rival_sprite/Iker.webp'),
         },
         {
-            'name':'Cristiano Ronaldo', 
-            'sprite': url_for('static', filename='images/rival_sprite/Cristiano.webp'), 
+            'name': 'Cristiano Ronaldo',
+            'sprite': url_for('static', filename='images/rival_sprite/Cristiano.webp'),
         },
         {
-            'name':'Santiago Abascal', 
-            'sprite':url_for('static', filename='images/rival_sprite/Abascal.webp'),
+            'name': 'Santiago Abascal',
+            'sprite': url_for('static', filename='images/rival_sprite/Abascal.webp'),
         },
         {
-            'name':'Estudiante DAW',
-             'sprite':url_for('static', filename='images/rival_sprite/Estudiante.webp'),
+            'name': 'Estudiante DAW',
+            'sprite': url_for('static', filename='images/rival_sprite/Estudiante.webp'),
         },
         {
-            'name':'Felipe VI', 
-            'sprite':url_for('static', filename='images/rival_sprite/Felipe_VI.webp'),},
+            'name': 'Felipe VI',
+            'sprite': url_for('static', filename='images/rival_sprite/Felipe_VI.webp'), },
         {
-            'name':'Francisco Franco', 
-            'sprite':url_for('static', filename='images/rival_sprite/Franco.webp'),
+            'name': 'Francisco Franco',
+            'sprite': url_for('static', filename='images/rival_sprite/Franco.webp'),
         },
         {
-            'name':'Guiri', 
-            'sprite':url_for('static', filename='images/rival_sprite/Guiri.webp'),
+            'name': 'Guiri',
+            'sprite': url_for('static', filename='images/rival_sprite/Guiri.webp'),
         },
         {
-            'name':'Ignatius', 
-            'sprite':url_for('static', filename='images/rival_sprite/Ignatius.webp'),
+            'name': 'Ignatius',
+            'sprite': url_for('static', filename='images/rival_sprite/Ignatius.webp'),
         },
         {
-            'name':'C++ Programmer', 
-            'sprite':url_for('static', filename='images/rival_sprite/Programador.webp'),
+            'name': 'C++ Programmer',
+            'sprite': url_for('static', filename='images/rival_sprite/Programador.webp'),
         },
         {
-            'name':'Pedro Sanchez', 
-            'sprite':url_for('static', filename='images/rival_sprite/Pedro_Sanchez.webp'),
+            'name': 'Pedro Sanchez',
+            'sprite': url_for('static', filename='images/rival_sprite/Pedro_Sanchez.webp'),
         },
         {
-            'name':'Mariano Rajoy', 
-            'sprite':url_for('static', filename='images/rival_sprite/Rajoy.webp'),
+            'name': 'Mariano Rajoy',
+            'sprite': url_for('static', filename='images/rival_sprite/Rajoy.webp'),
         },
         {
-            'name':'Pérez Reverte', 
-            'sprite':url_for('static', filename='images/rival_sprite/Reverte.webp'),
+            'name': 'Pérez Reverte',
+            'sprite': url_for('static', filename='images/rival_sprite/Reverte.webp'),
         }
     ]
-        
 
     colors = {
         'electric': 'yellow',
