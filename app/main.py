3a8e13ee7c4a66a3bb565ee52da41296fc2de284
mourfp <mourefp@gmail.com>
2025-11-21 17:43:39 +0100

    Inicio base de datos de explicacion

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from app.routes.home_routes import home_bp
 from app.routes.pokemon_routes import pokemon_bp
 from app.routes.battle_routes import battle_bp
+from app.database.db import db
 import logging
 
 
@@ -17,7 +20,14 @@ app.config["SESSION_PERMANENT"] = False     # Sesiones temporales
 app.config["SESSION_FILE_DIR"] = "./.flask_session"  # Carpeta donde se guardan
 app.secret_key = "clave_secreta"
 
-# Inicializar la extensión
+
+BASE_DIR=os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+BD_PATH=os.path.join(BASE_DIR,"data","**************.db") #y ponerlo abajo f"sqlite:///{BD_PATH}""
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:////data/battle.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+# db=SQLAlchemy(app) 
+
 Session(app)
 
 
@@ -25,6 +35,14 @@ Session(app)
 app.register_blueprint(home_bp, url_prefix = '/')
 app.register_blueprint(pokemon_bp, url_prefix = '/pokemons')
 app.register_blueprint(battle_bp, url_prefix = '/battle')
+
+
+@app.cli.command("create-tables") #Para ejecutar usar: ./.venv/Scripts/flask --app app.main create-tables
+def create_tables():
+    db.drop_all()
+    
+    db.create_all()
+    print("Tables created.")
 
 logging.basicConfig(level=logging.DEBUG)
 if __name__ == '__main__':
