ad7f18983829e0c0d0ac2d84a5069a0f7a44e800
Lucía Lagares Álvarez <lagaresalvarez8@gmail.com>
2025-11-24 17:59:17 +0100

    Añadido modelo de entrenador

--- app/models/trainer_db.py
+++ app/models/trainer_db.py
@@ -3,3 +3,7 @@ from app.database import db
 
 class TrainerDB(db.Model):
     __tablename__ = "TrainerDB"
+    
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False, unique = True)
+    password = db.Column(db.String(100), nullable = False)
